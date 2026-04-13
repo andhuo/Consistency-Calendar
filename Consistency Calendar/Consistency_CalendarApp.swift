f9d04e475fc4a4366492c9a42239918dfa63d78a
Andy Huoy <anhuo@unc.edu>
2026-04-13 15:07:44 -0400

    Initial Commit

---
+++ Consistency Calendar/Consistency_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Consistency_CalendarApp.swift
+//  Consistency Calendar
+//
+//  Created by Andy Huoy on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Consistency_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
